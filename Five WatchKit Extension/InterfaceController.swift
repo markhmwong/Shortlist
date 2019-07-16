9da637ee1db1a1d1338189fd567a23be61399f0d
markhmwong <markhmwong@gmail.com>
2019-07-16 19:34:42 +1000

    Initial Commit

---
+++ Five WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Five WatchKit Extension
+//
+//  Created by Mark Wong on 16/7/19.
+//  Copyright © 2019 Mark Wong. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
