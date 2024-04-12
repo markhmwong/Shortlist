1ecb0e7852c847c2c4828bc1726a4693d800d0d2
markhmwong <markhmwong@gmail.com>
2024-04-13 09:49:40 +1000

    reset model

--- Pods/Target Support Files/Pods-Shortlist/Pods-Shortlist-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shortlist : NSObject
-@end
-@implementation PodsDummy_Pods_Shortlist
-@end
--- Shortlist/TaskListView/TaskListViewController.swift
+++ Shortlist/TaskListView/TaskListViewController.swift
@@ -23,7 +23,7 @@ class TaskListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .blue
     }
     
     deinit {
